0cdd71f1fdd5d0c25751e35e9479c6394c74c163
devzhk <hzzheng@caltech.edu>
2022-03-16 19:54:16 -0700

    speed up sampling

--- algo/langevin.py
+++ algo/langevin.py
@@ -27,53 +27,56 @@ class LangevinMC(Optimizer):
                  lr=0.01,             # learning rate
                  beta_inv=0.01,       # inverse temperature parameter
                  sigma=1.0,           # variance of the Gaussian noise
-                 weight_decay=1.0):   # l2 penalty
+                 weight_decay=1.0,
+                 device=None):   # l2 penalty
         if lr < 0:
             raise ValueError('lr must be positive')
+        if device:
+            self.device = device
+        else:
+            self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         self.beta_inv = beta_inv
         self.lr = lr
+        self.sigma = sigma
+        self.temp = - math.sqrt(2 * beta_inv / lr) * sigma
         self.curr_step = 0
-        defaults = dict(lr=lr, sigma=sigma, weight_decay=weight_decay)
+        defaults = dict(weight_decay=weight_decay)
         super(LangevinMC, self).__init__(params, defaults)
-    #     self.init_map()
-    #
-    # def init_map(self):
-    #     self.mapping = dict()
-    #     index = 0
-    #     for group in self.param_groups:
-    #         for p in group['params']:
-    #             if p.grad is not None:
-    #                 num_param = p.numel()
-    #                 self.mapping[p] = [index, num_param]
-    #                 index += num_param
-    #     self.total_size = index
+
+    def init_map(self):
+        self.mapping = dict()
+        index = 0
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is not None:
+                    num_param = p.numel()
+                    self.mapping[p] = [index, num_param]
+                    index += num_param
+        self.total_size = index
 
     @torch.no_grad()
     def step(self):
         self.curr_step += 1
-        # beta = \sqrt
-        # beta = 1 / math.log(self.curr_step + 1) * self.beta
-        # noise = number of params
-        # # reshape
-        # mapping = p -> [a, b]
-        # p: noise[a, b]
-        beta_inv = self.beta_inv
+        if self.curr_step == 1:
+            self.init_map()
+
+        lr = self.lr
+        temp = self.temp
+        noise = temp * torch.randn(self.total_size, device=self.device)
+
         for group in self.param_groups:
-            lr = self.lr
-            sigma = group['sigma']
             weight_decay = group['weight_decay']
+
             params_with_grad = []
             d_p_list = []
             for p in group['params']:
                 if p.grad is not None:
                     params_with_grad.append(p)
-                    # noise = 0
-                    # std = math.sqrt(2 * beta_inv / lr) * sigma
-                    noise = - math.sqrt(2 * beta_inv / lr) * sigma * \
-                        torch.randn(p.shape, device=p.device)
-                    # noise = torch.normal(mean=torch.zeros_like(p), std=torch.full_like(p, fill_value=std))
+
+                    start, length = self.mapping[p]
+                    add_noise = noise[start: start + length].reshape(p.shape)
                     delta_p = p.grad
-                    delta_p = delta_p.add_(noise)
+                    delta_p = delta_p.add_(add_noise)
                     d_p_list.append(delta_p)
                     # p.add_(delta_p)
             lmc(params_with_grad, d_p_list, weight_decay, lr)
